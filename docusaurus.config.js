84424d226d63db9358fd33e39db9cedbd71cc86c
wallyuser <elvvisarg93@gmail.com>
2022-05-24 17:32:03 +0900

    deployment 구성

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,18 +6,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'WallyUser',
+  tagline: 'WallyUser Blog',
+  url: 'https://wallyuser.github.io',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wallyuser.github.io', // Usually your GitHub org/user name.
+  projectName: 'wallyuser.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false, 
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
